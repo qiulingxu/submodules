190e06e69fd59377542ab4e0edc182a6a87a690a
Qiuling Xu <simpleword2014@gmail.com>
2021-06-23 12:25:24 -0400

    last version check.

--- main.py
+++ main.py
@@ -18,13 +18,19 @@ from cl import EvalProgressPerSampleClassification as EPSP, \
     MetricClassification as MC, \
     ClassificationTrain as VT, \
     ClassificationMask as CM
-from cl.configs.cifar10_config import cifar10_incremental_config
-from cl.utils import get_config, save_config
-
+from cl.configs.imageclass_config import incremental_config
+from cl.utils import get_config, save_config, set_config
+from cl.algo import knowledge_distill_loss, EWC
+from functools import partial
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
+parser.add_argument("--dataset", default="cifar10")
+parser.add_argument("--net", default="ResNet18")
+parser.add_argument("--lwf", action="store_true")
+parser.add_argument("--ewc", action="store_true")
+parser.add_argument("--dev-scene", default="sequential")
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -45,6 +51,7 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
+
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
@@ -55,9 +62,15 @@ testset = torchvision.datasets.CIFAR10(
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=8)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+#classes = ('plane', 'car', 'bird', 'cat', 'deer',
+#           'dog', 'frog', 'horse', 'ship', 'truck')
 
+# configure the max step
+lwf = args.lwf
+ewc = args.ewc
+set_config("develop_assumption", args.dev_scene)
+set_config("classification_task", "domain_inc")
+config = incremental_config(args.dataset)
 
 # Model
 print('==> Building model..')
@@ -100,20 +113,45 @@ class ImageClassTraining(VT):
     def _model_process(self, model: nn.Module, key, step):
         if step == 0:
             self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
-        return model
+    
+    def pre_train(self):
+        self.ewcs = {}
+
+            
+
+    def post_task(self):
+        if ewc:
+            _ewc = EWC(self.curr_train_data, to_data_loader=partial(self.process_data, mode="test"))
+            _ewc.set_model(self.curr_model)
+            _ewc.eval()
+            self.ewcs[self.curr_order] = _ewc
+
 
     def _train(self, omodel, dataloader, prev_models, device, epoch):
         print('\nEpoch: %d' % epoch)
         model = torch.nn.DataParallel(omodel)
         train_loss = 0
         correct = 0
-        total = 0                     
+        total = 0
+        compare_pairs = []         
+        for compare_pair in self.taskdata.comparison:   
+            if compare_pair[-1] == self.current_k:
+                compare_pairs.append(compare_pair[0])             
         for batch_idx, (inputs, targets) in enumerate(dataloader):
             #print(inputs.shape, targets.shape)
             inputs, targets = inputs.to(device), targets.to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, targets)
+            loss_penalty = 0
+            for k in compare_pairs:
+                if lwf and len(prev_models)>0:
+                    klg_loss = knowledge_distill_loss(model, prev_models[k],)
+                    loss_penalty += klg_loss
+                if ewc and len(prev_models)>0:
+                    loss_penalty += self.ewcs[k].penalty(model)
+            loss_penalty /= len(compare_pairs)
+            loss += loss_penalty
             loss.backward()
             optimizer.step()
 
@@ -146,11 +184,13 @@ class ImageClassTraining(VT):
                 progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
-    def process_data(self, dataset, mode):
+    def process_data(self, dataset, mode, batch_size=None):
+        if batch_size is None:
+            batch_size = 128
         return torch.utils.data.DataLoader(
             dataset, batch_size=128, shuffle=True, num_workers=2)
 
-config = cifar10_incremental_config()
+
 ICD = config["taskdata"]
 epsp = EPSP(MC(), device, max_step= 6)
 #epsp.add_data(name="test",data=fd_test)
